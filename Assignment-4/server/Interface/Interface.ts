b21ae8800cc3dff48a4f9be4118fd94ca90ba1df
Faizahmedsf <121433435+Faizahmedsf@users.noreply.github.com>
2023-03-20 10:58:45 +0530

    Initial Commit

---
+++ Assignment-4/server/Interface/Interface.ts
@@ -0,0 +1,11 @@
+export interface user {
+    id?:string ,
+    first_name:string ,
+    DOB:string ,
+    middle_name:string ,
+    last_name:string ,
+    email:string ,
+    phone_number:string | number ,
+    role: string,
+    address: string,
+}
