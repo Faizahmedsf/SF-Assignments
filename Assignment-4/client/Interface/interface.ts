5eef340ccb4c18d14c81d60cc1140d11217dd93c
Faizahmedsf <121433435+Faizahmedsf@users.noreply.github.com>
2023-03-20 11:01:25 +0530

    Update interface.ts

--- Assignment-4/client/Interface/interface.ts
+++ Assignment-4/client/Interface/interface.ts
@@ -1,17 +1,3 @@
-// export class User2 { 
-//     constructor(
-//         id: string,
-//         first_name: string,
-//         DOB: string,
-//         middle_name: string,
-//         last_name: string,
-//         email: string,
-//         phone_number: string,
-//         role: string,
-//         address: string
-//     )
-//     {}
-// }
 
 export enum userRole2 {
     SuperAdmin = "SuperAdmin",
