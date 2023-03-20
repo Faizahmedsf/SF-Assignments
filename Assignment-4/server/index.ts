3ae6438f88ce13f4ba56e8d861f4a9263ca3bd60
Faizahmedsf <121433435+Faizahmedsf@users.noreply.github.com>
2023-03-20 10:54:05 +0530

    Initial Commit

---
+++ Assignment-4/server/index.ts
@@ -0,0 +1,27 @@
+import express , {Request , Response} from 'express';
+import dotenv from 'dotenv';
+import indexRouter from './Router/Router.js';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+// import bodyParser  from "body-parser"
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors({ 
+  origin: '*'
+}));
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended:true }))
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('lets run the game');
+});
+  
+app.listen(port, () => {
+  console.log(`[server]:  Server is running at http://localhost:${port}`);
+}); 
+
+app.use(indexRouter) 
