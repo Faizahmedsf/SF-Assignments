2564def8243f54c5ccef5cdb8aa13e846e3d6957
Faizahmedsf <121433435+Faizahmedsf@users.noreply.github.com>
2023-04-03 16:15:46 +0530

    Assignment 7

---
+++ Assignment-7/Server/Interface/interface.ts
@@ -0,0 +1,12 @@
+type RoleEnum = "Admin | SuperAdmin | Subscriber"
+
+
+export interface user {
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    email: string,
+    phone: number,
+    role: boolean
+    address: string,
+}
